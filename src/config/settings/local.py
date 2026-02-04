a4649f846051eda6d2bf6eb719e63975714c87fe
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2026-02-04 17:53:06 +0900

    fix::配色調整

--- src/config/settings/local.py
+++ src/config/settings/local.py
@@ -4,3 +4,14 @@ DEBUG = True
 # 開発のために上書きしてAI機能をOFFにしておく
 AI_ENABLED = False
 
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "greyson-pointilla-cindi.ngrok-free.dev",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://greyson-pointilla-cindi.ngrok-free.dev",
+]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
